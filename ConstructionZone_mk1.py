d78a51e0dc1ef3ef926c467f8cf160098e01d05a
Trevor La Pay <trevor.n.lapay@gmail.com>
2018-12-02 13:16:43 -0700

    26.6% accuracy using CNN

--- ConstructionZone_mk1.py
+++ ConstructionZone_mk1.py
@@ -6,8 +6,8 @@ import shutil
 import pandas as pd
 from keras.preprocessing.image import ImageDataGenerator
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
-from keras.optimizers import SGD
+from keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout, BatchNormalization, Activation
+from keras.optimizers import SGD, Adam
 
 all_classes = ["Nucleoplasm", "Nuclear membrane", "Nucleoli", "Nucleoli fibrillar center", "Nuclear speckles",
                "Nuclear bodies", "Endoplasmic reticulum",  "Golgi apparatus",
@@ -21,6 +21,7 @@ green_only_dir='all/train/green_only'
 train_dir = 'all/train'
 valid_dir = 'all/valid'
 test_dir = 'all/test'
+percent_validate = .1
 
 # Move files into respective directories for each class.
 # Only do this once (luckily...)
@@ -29,67 +30,74 @@ def processFilesForImageDataGenerator():
     for index, row in df.iterrows():
         for filename in os.listdir(green_only_dir):
             if filename.startswith(row['Id']):
-                for num in row['Target'].split():
-                    SourceFolder = os.path.join(green_only_dir, filename)
-                    shutil.copy(SourceFolder, os.path.join(green_only_dir, num))
+                num = row['Target'].split()
+                SourceFolder = os.path.join(green_only_dir, filename)
+                shutil.copy(SourceFolder, os.path.join(train_dir, num[0]))
 
 
+
+def moveTrainFilesToValidationDir():
+    for n in range(27):
+        numDir = os.path.join(train_dir, str(n))
+        for count, file in enumerate(os.listdir(numDir)):
+            if count/len(os.listdir(numDir)) < percent_validate:
+                newfile = os.path.join(os.path.join(valid_dir, str(n)), file)
+                shutil.move(os.path.join(numDir, file), newfile)
 
 def readTrainingFile():
     return pd.read_csv("all/train.csv")
 
 def loadAll():
-    train_datagen = ImageDataGenerator(
-            shear_range=0.2,
-            zoom_range=0.2,
-            horizontal_flip=True)
+    train_datagen = ImageDataGenerator()
 
     test_datagen = ImageDataGenerator()
 
     train_generator = train_datagen.flow_from_directory(
             train_dir,
-            target_size=(150, 150),
+            target_size=(256, 256),
             batch_size=32,
             class_mode='categorical')
 
     validation_generator = test_datagen.flow_from_directory(
             valid_dir,
-            target_size=(150, 150),
+            target_size=(256, 256),
             batch_size=32,
             class_mode='categorical')
 
     test_generator = test_datagen.flow_from_directory(
             directory=test_dir,
-            target_size=(150, 150),
-            color_mode="grayscale",
+            target_size=(256, 256),
+            color_mode="rgb",
             batch_size = 1,
             class_mode = "categorical",
             shuffle = True,
             seed = 42)
 
+
     model = Sequential()
-    # input: 100x100 images with 3 channels -> (100, 100, 3) tensors.
-    # this applies 32 convolution filters of size 3x3 each.
-    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
-    model.add(Conv2D(32, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(Conv2D(64, (3, 3), activation='relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(0.25))
-
+    model.add(Conv2D(32, kernel_size=(4, 4), strides=(1, 1), input_shape=(256, 256, 3)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.3))
+    model.add(Conv2D(64, kernel_size=(4, 4), strides=(1, 1)))
+    model.add(BatchNormalization())
+    model.add(Activation('relu'))
+    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
+    model.add(Dropout(0.3))
     model.add(Flatten())
-    model.add(Dense(256, activation='relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(28, activation='softmax'))
-
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    model.compile(loss='categorical_crossentropy', optimizer=sgd, metrics=['accuracy'])
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(1024))
+    model.add(Activation('relu'))
+    model.add(Dropout(0.3))
+    model.add(Dense(len(all_classes)))
+    model.add(Activation('softmax'))
+    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.8, nesterov=True)
+    model.compile(loss='categorical_crossentropy', optimizer='adagrad', metrics=['accuracy'])
     model.fit_generator(
             generator=train_generator,
-            steps_per_epoch=50,
+            steps_per_epoch=100,
             epochs=20,
             validation_data=validation_generator,
             validation_steps=800)
@@ -117,7 +125,6 @@ def serialize(model):
 
 def main():
     loadAll()
-    # processFilesForImageDataGenerator()
 
 if __name__ == "__main__": main()
 
